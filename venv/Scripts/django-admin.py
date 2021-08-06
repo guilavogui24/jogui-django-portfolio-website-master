7f9be16df058f34e9d3d864ff79e9339c6b206e2
guilavogui24 <guilavoguijoseph@gmail.com>
2021-08-06 17:57:25 +0100

    first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\HP\Documents\PYTHON\django-portfolio-website-master\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
